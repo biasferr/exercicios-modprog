d4b3edd312d7b40259e822a70bff079a60d94f73
biasferr <bia.sferr07@gmail.com>
2026-04-09 12:54:16 -0300

    Add files via upload

---
+++ exercicios while.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 12:38:14 2026
+
+@author: PC25
+"""
+
+def calcula_media(nota1,nota2):
+    media= (nota1 + nota2)/2
+    return media
+
+
+alunos=4
+cont= 0
+aprovados= 0
+
+while cont<alunos:
+    nota1 = float(input('Digite a sua primeira nota: '))
+    nota2 = float(input('Digite a sua segunda nota: '))
+    media= calcula_media(nota1, nota2)
+    print (f'A sua média é: {media:.2f}')
+    if media >= 5:
+        print ('APROVADO')
+        aprovados +=1
+    else:
+        print ('REPROVADO')
+    cont += 1
+    
+    
+print (f'Número de alunos aprovados: {aprovados}')
